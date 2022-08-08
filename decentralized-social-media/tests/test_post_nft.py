0de781beaa4e7756f9a9a15d5c5ad2ae39e969bd
Yash Tarkar <ytarkar138@gmail.com>
2022-08-08 20:22:18 +0530

    'post-nft-inherited'

--- decentralized-social-media/tests/test_post_nft.py
+++ decentralized-social-media/tests/test_post_nft.py
@@ -1,1 +1,7 @@
 # write tests for PostNft contract here
+
+"""
+use the SocialMedia contract to call PostNft functions
+just check if the constructor has initialized counter value correctly
+as that is the only public function
+"""
