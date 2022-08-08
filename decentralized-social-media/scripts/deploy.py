80d60122e735701ac907abf41ed93ccd41a40092
Yash Tarkar <ytarkar138@gmail.com>
2022-08-08 20:15:05 +0530

    'post-nft-inherited'

--- decentralized-social-media/scripts/deploy.py
+++ decentralized-social-media/scripts/deploy.py
@@ -4,9 +4,7 @@ This script will deploy all our contracts
 
 # things to do -
 """
-1. deploy the PostNft contract first
-3. store the address of PostNft contract
-3. deploy SocialMedia contract and pass the PostNft address in the constructor
+1. deploy SocialMedia contract by passing the mint fee to it
 
 Note: make sure you verify the contracts on etherscan while deploying
 verifying makes our contracts easily traceable on etherscan
