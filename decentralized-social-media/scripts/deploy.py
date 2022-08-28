892db5760c938cdf3ceee26b0405156e895fcc49
SacCrypt <sachinfulsunge1@gmail.com>
2022-08-28 21:06:48 +0530

    added function

--- decentralized-social-media/scripts/deploy.py
+++ decentralized-social-media/scripts/deploy.py
@@ -10,12 +10,23 @@ Note: make sure you verify the contracts on etherscan while deploying
 verifying makes our contracts easily traceable on etherscan
 """
 import os
+import json
 from dotenv import load_dotenv
-from brownie import Wei, accounts, SocialMedia
+from brownie import Wei, accounts, SocialMedia, chain
 
 load_dotenv()
 
 
 def main():
-    print(accounts[0])
     txn = SocialMedia.deploy((0.001), {"from": accounts[0]})
+    store_add_abi(txn)
+
+def store_add_abi(txn):
+    jsonify_abi = json.loads(txn.abi)
+    _dict = {}
+    _dict[f"{chain.id}": txn.address]
+    jsonify_address = json.loads(_dict)
+    with open("../../decentralized-social-media-client/constants/abi.json", "w") as abi:
+        json.dump(jsonify_abi, abi)
+    with open("../../decentralized-social-media-client/constants/addresses.json", "w") as add:
+        json.dump(jsonify_address, add)
